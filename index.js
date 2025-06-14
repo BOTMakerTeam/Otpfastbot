6905e09c046164929e1842b3bcf2051e968b1ced
BOTMakerTeam <adsgrower2@gmail.com>
2025-06-15 00:35:05 +0530

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const chromium = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer-core');
+const fs = require('fs');
+const { extractSMS } = require('./forwarder');
+const config = require('./config');
 
-const browser = await puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,  // यही path चलाता है lambda Chrome
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true
-});
+(async () => {
+    console.log("[*] Launching AWS-compatible Chromium...");
+
+    const browser = await puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+        ignoreHTTPSErrors: true
+    });
+
     const page = await browser.newPage();
     const cookies = JSON.parse(fs.readFileSync('./cookies.json', 'utf8'));
     await page.setCookie(...cookies);
